d4d15ced3f254fe7e540fdf6c009e110a7fbb49a
rdzur <rdzur@bhinc.com>
2016-05-02 17:39:05 -0400

    Updated exercise_4.js

--- js/exercise_4.js
+++ js/exercise_4.js
@@ -124,7 +124,7 @@ function getDirections(frm, to){
      $('#directions').fadeIn(400, function(){
         var summary = data.trip.summary
         $('#summary').empty();
-        $('#distance').txt(Math.round(summary.length * 100) / 100) + data.trip.unitys; 
+        $('#distance').text(Math.round(summary.length * 100) / 100) + data.trip.unitys; 
         $('#time').text((Math.round(summary.time / 60 *100) / 100) + 'min');
      })
      
